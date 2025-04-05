6b7ae1c688092f738cc67f9d0d3f5db99c80b1aa
Claudia_Ines <claudiainees@hotmail.com>
2025-04-05 11:45:54 -0300

    Adjusting page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Curso from "./curso/page";
-import Projeto from "@/projeto/page";
-import ProjetoHome from "@/projeto/ProjetoHome"
+import Projeto from "@/app/projeto/page";
+import ProjetoHome from "@/app/projeto/detalhes/page"
 import HomePage from "@/home/page";
 export default function Home() {
 
