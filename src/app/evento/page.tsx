0acac025a0a612e3c36f3da089e4a0ec9e17ece5
Dermeval Neves <dermeval_filho@hotmail.com>
2025-06-05 12:34:55 -0300

    Criar Pagina

--- src/app/evento/page.tsx
+++ src/app/evento/page.tsx
@@ -1,7 +1,6 @@
 
 "use client"
-import 'dotenv/config'
-
+//import 'dotenv/config'
 
 import React, { useState, useEffect, useCallback } from 'react';
 import { Button } from "@/components/ui/button";
