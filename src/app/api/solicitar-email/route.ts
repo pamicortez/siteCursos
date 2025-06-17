fbee4cf785585f843dd4d0670779b91e9ba8b8d7
Vanderleicio <vanderleiciojr397@gmail.com>
2025-06-17 10:02:21 -0300

    fix: verificação do e-mail de envio
    Verifica se o e-mail já está cadastrado antes de enviar o código

--- src/app/api/solicitar-email/route.ts
+++ src/app/api/solicitar-email/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
 import { randomInt } from 'crypto';
 import nodemailer from 'nodemailer';
@@ -10,7 +11,17 @@ export async function POST(req: Request) {
   const code = randomInt(100000, 999999).toString();
   const expiresAt = new Date(Date.now() + 10 * 60 * 1000); // Expira em 10 min
 
-  // Salva o código no banco
+  const email_unico = await prisma.usuario.findUnique( { where: { email: email } });
+  if (email_unico){
+      return NextResponse.json({error: 'Email já cadastrado'}, {status: 409})
+    }
+  
+  await prisma.emailVerificationCode.deleteMany({
+    where: {
+      email: email,
+    },
+  });
+  
   await prisma.emailVerificationCode.create({
     data: {
       email,
