b90285aef5283b434364c815df78845a3f172d25
Wagner Alexandre <wagner.afjr@gmail.com>
2025-06-17 01:22:09 -0300

    fix: bloqueio de login não autorizado

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -18,16 +18,8 @@ const handler = NextAuth( {
                     where: { email: credentials?.username },
                 });
 
-                if ((user && bcrypt.compareSync(credentials?.password || "", user.senha))) {
-                    if ((user.tipo=='Normal') || (user.tipo=='Super')) {
+                if ((user && bcrypt.compareSync(credentials?.password || "", user.senha))){
                         return { id: user.id.toString(), name: user.Nome, email: user.email };
-                    }
-                    if (user.tipo=='Bloqueado'){
-                        throw new Error("Sua conta está bloqueada.");
-                    }
-                    if (user.tipo=='Pendente'){
-                        throw new Error("Sua conta está pendente de aprovação.");
-                    }
                 }
 
                 return null;
