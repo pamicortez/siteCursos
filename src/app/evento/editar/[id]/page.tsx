720aaacbef568a5ad4d64f868b9f6f9b07b75d2d
Dermeval Neves <dermeval_filho@hotmail.com>
2025-07-15 20:28:08 -0300

    ID Evento

--- src/app/evento/editar/[id]/page.tsx
+++ src/app/evento/editar/[id]/page.tsx
@@ -3,7 +3,7 @@
 import Evento from "../../../page"; // Importa o componente do formulário que está em 'src/app/evento/page.tsx'
 
 export default function EditarEventoPage() {
-    console.log("URL de destino:", url); // Verificação de URL,M par aonde está indo
+   // console.log("URL de destino:", url); // Verificação de URL,M par aonde está indo
   // Simplesmente renderizamos o componente 'Evento'.
   // Ele é inteligente o suficiente para ler o 'id' do URL e entrar no modo de edição.
   return <Evento />;
