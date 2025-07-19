158e0e3382b9a61d176f865893887da9e4b70bcd
Washington Oliveira Júnior <wdeoliveiraj@gmail.com>
2025-07-18 22:13:10 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["i.imgur.com", "via.placeholder.com", "your-image-host.com"], // Adicione domínios confiáveis
   },
